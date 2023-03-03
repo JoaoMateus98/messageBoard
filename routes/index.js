be3378a28a00d741e2f728fbf4297d2c664b0459
JoaoMateus98 <joaobsu98@gmail.com>
2023-03-03 17:46:17 -0500

    list of messages now shows up in index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    }),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    }),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messaging App", messages: messages });
 });
 
 module.exports = router;
